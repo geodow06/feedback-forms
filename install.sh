b7f1311f343611c73588adcddb1acc3f3a7b2f13
Jordan <jorda.hignett1@googlemail.com>
2019-03-26 10:34:52 +0000

    Update install.sh
    changed from imallinson repo to georges repo

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/imallinson/feedback-forms-accounts
-git clone https://github.com/imallinson/feedback-forms-submit
-git clone https://github.com/imallinson/feedback-forms-consumer
-git clone https://github.com/imallinson/feedback-forms-front
-git clone https://github.com/imallinson/feedback-forms-gateway
-git clone https://github.com/imallinson/feedback-forms-retriever
-git clone https://github.com/imallinson/feedback-forms-cohorts
+git clone https://github.com/geodow06/feedback-forms-accounts
+git clone https://github.com/geodow06/feedback-forms-submit
+git clone https://github.com/geodow06/feedback-forms-consumer
+git clone https://github.com/geodow06/feedback-forms-front
+git clone https://github.com/geodow06/feedback-forms-gateway
+git clone https://github.com/geodow06/feedback-forms-retriever
+git clone https://github.com/geodow06/feedback-forms-cohorts
 
 docker-compose up -d
