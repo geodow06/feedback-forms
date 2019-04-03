0141f3c2438c722141d3d401fc5807d79666a708
Jordan <jorda.hignett1@googlemail.com>
2019-04-03 09:14:32 +0100

    Updated to our version

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/geodow06/feedback-forms-accounts
-git clone https://github.com/geodow06/feedback-forms-submit
 git clone https://github.com/geodow06/feedback-forms-consumer
 git clone https://github.com/geodow06/feedback-forms-front
 git clone https://github.com/geodow06/feedback-forms-gateway
-git clone https://github.com/geodow06/feedback-forms-retriever
-git clone https://github.com/geodow06/feedback-forms-cohorts
+git clone https://github.com/geodow06/feedback-form-average
+git clone https://github.com/geodow06/feedback-forms-mail
 
 docker-compose up -d
