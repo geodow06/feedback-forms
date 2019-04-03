b73b258e3e436e683d785b2497941d4a86417c2a
geodow06 <47025866+geodow06@users.noreply.github.com>
2019-04-03 09:55:35 +0100

    Added prerequisites

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+curl https://get.docker.com | sudo bash
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 git clone https://github.com/geodow06/feedback-forms-consumer
 git clone https://github.com/geodow06/feedback-forms-front
